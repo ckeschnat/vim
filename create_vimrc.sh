d34bf3c72ac995478d530deccef9d5fc87391413
Chris Keschnat <c.keschnat@vervum.de>
2013-06-12 10:14:14 +0200

    Default is now to disable plugins

--- create_vimrc.sh
+++ create_vimrc.sh
@@ -4,8 +4,8 @@ function! MySys()
     return "unix"
 endfunction
 
-" Uncomment to disable loading of plugins
-" let g:disable_all_plugins = 1
+" Disable loading of plugins
+let g:disable_all_plugins = 1
 
 source ~/.vim/vimrc
 DELIM
